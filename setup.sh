dff8e017e9c7a8a04be1e0a83df4c9ae6eee0747
Arunav Gupta <centaurus.ab@gmail.com>
2020-07-07 22:25:57 -0700

    typo

--- setup.sh
+++ setup.sh
@@ -14,23 +14,24 @@ sudo apt update
 sudo apt -y dist-upgrade
 sudo apt-get install -y openjdk-8-jre-headless xvfb libxi6 libgconf-2-4 zip
 
-sudo wget -N https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -P ~/
-sudo dpkg -i --force-depends ~/google-chrome-stable_current_amd64.deb
-sudo apt-get -f install -ydpkg -i --force-depends ~/google-chrome-stable_current_amd64.deb
+sudo curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
+sudo echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
+sudo apt-get -y update
+sudo apt-get -y install google-chrome-stable
 
 echo Google Chrome installed!
 
-sudo wget -N https://chromedriver.storage.googleapis.com/2.9/chromedriver_linux64.zip -P ~/
-sudo unzip ~/chromedriver_linux64.zip -d ~/
-sudo rm ~/chromedriver_linux64.zip
-sudo mv -f ~/chromedriver /usr/local/share/
-sudo chmod +x /usr/local/share/chromedriver
-sudo ln -s /usr/local/share/chromedriver /usr/local/bin/chromedriver
+wget -N https://chromedriver.storage.googleapis.com/79.0.3945.36/chromedriver_linux64.zip -P ~/
+unzip ~/chromedriver_linux64.zip -d ~/
+rm ~/chromedriver_linux64.zip
+sudo mv -f ~/chromedriver /usr/local/bin/chromedriver
+sudo chown root:root /usr/local/bin/chromedriver
+sudo chmod 0755 /usr/local/bin/chromedriver
 
 echo ChromeDriver installed!
 
+sudo apt-get --fix-broken-install
 sudo apt install python3-pip
-sudo pip3 install selenium
 pip3 install --upgrade pip
 pip3 install -r requirements.txt
 
