7ce59b39c4a834133b2bbd7a2b0b28ce3a2c7720
Arunav Gupta <centaurus.ab@gmail.com>
2020-07-07 20:51:33 -0700

    print statements

--- setup.sh
+++ setup.sh
@@ -14,8 +14,12 @@ wget https://chromedriver.storage.googleapis.com/2.37/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
 sudo mv chromedriver /usr/bin/chromedriver
 
+echo ChromeDriver installed!
+
 curl https://intoli.com/install-google-chrome.sh | bash
 sudo mv /usr/bin/google-chrome-stable /usr/bin/google-chrome
+
+echo Google Chrome installed!
 
 sudo apt install python3-pip
 pip install --upgrade pip
