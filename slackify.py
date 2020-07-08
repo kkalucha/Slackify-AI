e83cde38cbb7f80443342fe9cc616c4475cdac1e
kkalucha <kanav.kalucha@gmail.com>
2020-07-07 18:19:37 -0700

    copied echobot

--- slackify.py
+++ slackify.py
@@ -1,6 +1,25 @@
 import os
+from fbchat import log, Client
 
 username = os.environ.get('SLACKIFY_USERNAME')
 password = os.environ.get('SLACKIFY_PASSWORD')
 
-print(username, password)
+# Subclass fbchat.Client and override required methods
+class EchoBot(Client):
+    def tag_all():
+        pass
+
+    function_lib = {"all" : tag_all}
+
+    def onMessage(self, author_id, message_object, thread_id, thread_type, **kwargs):
+        self.markAsDelivered(thread_id, message_object.uid)
+        self.markAsRead(thread_id)
+
+        log.info("{} from {} in {}".format(message_object, thread_id, thread_type.name))
+
+        # If you're not the author, echo
+        if author_id != self.uid:
+            self.send(message_object, thread_id=thread_id, thread_type=thread_type)
+
+client = EchoBot(str(username), str(password))
+client.listen()
