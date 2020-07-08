e4eb77b7f8f70210e7fd52f30239ae9b39db7e62
Arunav Gupta <centaurus.ab@gmail.com>
2020-07-07 21:37:40 -0700

    typo

--- selenium_test.py
+++ selenium_test.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 CHROMEDRIVER_PATH = '/usr/local/bin/chromedriver'
-WINDOW_SIZE = "1920,1080"chrome_options = Options()
+WINDOW_SIZE = "1920,1080"
+chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
 chrome_options.add_argument('--no-sandbox')driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH,
