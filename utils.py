9c74fe1ba4f124c8309c2ecba3046bab80f5de34
Amogh Ayalasomayajula <27amogh@gmail.com>
2020-07-09 01:36:59 -0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -36,13 +36,17 @@ def ap_comment(client, author_id, message_object, thread_id, thread_type):
     client.send(Message(text="yOu CaN't AuToMaTe HeAlThCaRe"), thread_id=thread_id, thread_type=thread_type)
     
 def sully_comment(client, author_id, message_object, thread_id, thread_type):
-    client.send(Message(text="i Am NoT gAY gUyS I swEAr"), thread_id=thread_id, thread_type=thread_type)
+    client.send(Message(text="i Am NoT ___ gUyS I swEAr"), thread_id=thread_id, thread_type=thread_type)
+    
+def pranshu_comment(client, author_id, message_object, thread_id, thread_type):
+    client.send(Message(text="Pranshu is a student at the University of Illinois Urbana-Champaign studying Computer Science and Statistics. My interests lie in High Performance Computing (HPC) and in AI/Deep Learning. Recently I attended the Super Computing 19 conference where I represented my school as a member of the University of Illinois Student Cluster Competition team; our team won 2nd place nationwide. I've recently also won 2nd place at the National Center for Supercomputing Applications Deep Learning Hackathon. At the Technology Student Association’s national conference in June, 2019, my team won 1st place out of over 75 teams in a research presentation competition on exploring a novel application of artificial intelligence in a domain field (website: pinkai.tech). I am an enthusiastic candidate for any role relating to HPC or Deep Learning; I hope to expand my skill set in the summer of 2020 through an internship at a company focusing on these disciplines. "), thread_id=thread_id, thread_type=thread_type)
 
 command_lib = {"all" : {"func" : tag_all}, 
                 "kick" : {"func" : kick}, 
                 "meet" : {"func" : hear_meet},
                 "laugh" : {"func" : laugh},
                "sully" : {"func" : sully_comment},
+               "pranshu" : {"func" : pranshu_comment},
                 "ap" : {"func" : ap_comment}}
 
 def command_handler(client, author_id, message_object, thread_id, thread_type):
