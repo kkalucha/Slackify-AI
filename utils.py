2b6dd6059c60f4699e3573c24e9091da80d26d43
Rishi Chillara <rishichillara@rishis-mbp.lan>
2020-07-29 23:14:21 -0700

    added coin flip hotfix

--- utils.py
+++ utils.py
@@ -498,12 +498,11 @@ def scenesfromahat(client, author_id, message_object, thread_id, thread_type):
 def coin_flip(client, author_id, message_object, thread_id, thread_type):
     coin_flip = random.choice([1,2])
     if coin_flip == 1:
-        action_queue.put(Action(client, 'message', thread_id, thread_type, text="It turned up heads!")
+        action_queue.put(Action(client, 'message', thread_id, thread_type, text="It turned up heads!"))
     else:
         action_queue.put(Action(client, 'message', thread_id, thread_type, text="You got tails!"))
+
         
-
-
 command_lib = {"all" : {"func" : tag_all, "description" : "Tags everyone in the chat", private="N"}, 
                 "kick" : {"func" : kick, "description" : "Kicks the specified user from the chat", private="N"}, 
                 "meet" : {"func" : hear_meet, "description" : "Creates poll to decide on time for given date", private="N"},
